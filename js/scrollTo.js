dbb7f4b7bb1f0a94734c5ffb984b89403d11de28
inbalchen <inbalchenz@gmail.com>
2023-03-14 15:28:58 +0200

    scroll links functionality

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -1,11 +1,24 @@
-$(document).ready(function () {
-let imageHeight = $('.home__container').height();
-  $("#about-me-link").click(function () {
-    $("body,html").animate(
-      {
-        scrollTop: imageHeight,
-      },
-      400
-    );
-  });
-});
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function(){
+  let currentSection = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if(pageYOffset >= (sectionTop - sectionHeight / 2)){
+      currentSection = section.getAttribute('id'); 
+      
+    }
+  })
+
+  navLinks.forEach(link => {
+    link.classList.remove('active')
+    if(link.classList.contains(currentSection)) {
+      link.classList.add('active')
+    }
+  })
+}) 
+
+
+
