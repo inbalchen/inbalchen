ee571c5840c5d0c8e8a05d03d48a39cc4c2088fb
inbalchen <inbalchenz@gmail.com>
2023-03-21 11:12:24 +0200

    added progress bar

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,20 @@
 const form = document.getElementById("form");
 const result = document.getElementById("result");
+const progressBar = document.querySelector(".container__progressbar");
+const formGroup = document.querySelectorAll(".form__group");
 
 form.addEventListener("submit", function (e) {
   const formData = new FormData(form);
   e.preventDefault();
-  var object = {};
+  let object = {};
   formData.forEach((value, key) => {
     object[key] = value;
   });
-  var json = JSON.stringify(object);
-  result.innerHTML = "Please wait...";
+  let json = JSON.stringify(object);
+  result.innerHTML = "";
+  result.className = "";
+  removeFormGroupFromDOM("none");    
+  progressBar.style.display = "flex";
 
   fetch("https://api.web3forms.com/submit", {
     method: "POST",
@@ -21,25 +26,37 @@ form.addEventListener("submit", function (e) {
   })
     .then(async (response) => {
       let json = await response.json();
+      await delay(6000);
+      progressBar.style.display = "none";
       if (response.status == 200) {
         result.innerHTML = json.message;
-        result.classList.remove("text-gray-500");
-        result.classList.add("text-green-500");
+        result.classList.add('success');
       } else {
         console.log(response);
         result.innerHTML = json.message;
-        result.classList.remove("text-gray-500");
-        result.classList.add("text-red-500");
+        result.classList.add('error');
       }
     })
     .catch((error) => {
       console.log(error);
+      progressBar.style.display = "none";
       result.innerHTML = "Something went wrong!";
+      result.classList.add('error');
     })
     .then(function () {
       form.reset();
       setTimeout(() => {
-        result.style.display = "none";
+        result.innerHTML = "";
+        progressBar.style.display = "none";
+        removeFormGroupFromDOM("block");
       }, 5000);
     });
 });
+
+const removeFormGroupFromDOM = (visible) => {
+  formGroup.forEach(group => {
+    group.style.display = visible;
+  })
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
